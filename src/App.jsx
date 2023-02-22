b6502b812f3acbc8b7e9b47b7ef16fa1bf10fcf0
Stephanie Hernandez <stephernandez831@gmail.com>
2023-02-21 18:51:33 -0800

    Updated with title and description

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ const App = () => {
 
   return (
     <div className="App">
-
-      
+      <h1>Stephanie's Class Schedule 🗓️</h1>
+      <h2>This calendar will display Stephanie's weekly class schedule.</h2>
     </div>
   )
 }
