c38677538a44b367f877940408ba43d52dc914fb
Stephanie Hernandez <stephernandez831@gmail.com>
2023-02-21 18:55:46 -0800

    Created Calendar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css';
+import Calendar from './components/Calendar'
 
 const App = () => {
 
@@ -6,6 +7,7 @@ const App = () => {
     <div className="App">
       <h1>Stephanie's Class Schedule 🗓️</h1>
       <h2>This calendar will display Stephanie's weekly class schedule.</h2>
+      <Calendar />
     </div>
   )
 }
